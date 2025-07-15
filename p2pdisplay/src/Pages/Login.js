8f0786b801c81cc7affebbbce401af3afba160dc
Ignacio Yockers <ignacio.l.yockers-1@ou.edu>
2025-07-15 20:35:51 +0200

    login page styling done until ecrytpion options are added

--- p2pdisplay/src/Pages/Login.js
+++ p2pdisplay/src/Pages/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Navigate, useNavigate } from "react-router-dom"
 import './Login.css'
+import logo from '../logo.png'
 
 function Login() {
 
@@ -33,12 +34,14 @@ function Login() {
     return (
         <div className="Container">
             <div className = "SignInBlock">
+                <img src={logo} className="Logo" alt="logo" />
+                <h4 className="Title">Not Secure Secure Messaging</h4>
                 <form onSubmit={signin} className="InputContainer">
                     <input id="Username" name="username" className="textbox" type="text" placeholder="Username" 
                     title="Name that will be displayed" required="" autoFocus="" onChange={changeUser}/>
                     <input id="Password" name="password" className="textbox" type="password" placeholder="Password" 
                     title="Password" required="" autoFocus="" onChange={changePassword}/>
-                    <input className="button" type="submit" value="Sign In" title="submit"></input>
+                    <input className="enterButton" type="submit" value="Sign In" title="submit"></input>
                 </form>
             </div>
         </div>
